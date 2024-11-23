1497ce91eac3676855f1aa72003ecbe66775cc53
Jesus <castrojesus39@gmail.com>
2024-11-22 22:22:27 -0800

    added method for counting each word

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+def count_words(file):
+    wordCount = file.split()
+    return len(wordCount)
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
+
+        print(count_words(file_contents))
 
 if __name__ == "__main__":
     main()
