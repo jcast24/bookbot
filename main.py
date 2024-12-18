72db6ff895733f930fd5791685f94ce66f6de10f
Jesus <castrojesus39@gmail.com>
2024-12-17 23:21:12 -0800

    method created for counting each character from the text

--- main.py
+++ main.py
@@ -2,12 +2,26 @@ def count_words(file):
     wordCount = file.split()
     return len(wordCount)
 
+def count_characters(file):
+    count = {}
+    lowered_file_text = file.lower()
+    for letter in lowered_file_text:
+        if letter.isalpha():
+            if letter in count.keys():
+                count[letter] +=1 
+            else:
+                count[letter] = 1
+    return count
+
+
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-        print(count_words(file_contents))
+
+        # print(count_words(file_contents))
+        print(count_characters(file_contents))
 
 if __name__ == "__main__":
     main()
